7dd5d02d6828bedb0e4d999fb0cacc371bdb260e
sreisner <shawn.reisner@gmail.com>
2018-09-22 08:22:13 -0500

    Update product api to return update fields

--- db/models/product.js
+++ db/models/product.js
@@ -1,9 +1,24 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const ProductImageSchema = Schema({
+  src: {
+    type: String,
+    required: true,
+  },
+  alt: {
+    type: String,
+    required: true,
+  },
+});
+
 const ProductSchema = Schema(
   {
     _id: {
+      type: Number,
+      required: true,
+    },
+    categoryId: {
       type: Number,
       required: true,
     },
@@ -11,61 +26,45 @@ const ProductSchema = Schema(
       type: String,
       required: true,
     },
-    categoryId: {
-      type: Number,
-      required: true,
-    },
     description: {
       type: String,
       required: true,
     },
-    imageUrls: {
-      type: [String],
-      required: true,
-    },
-    duration: {
-      type: Number,
-      required: false,
-    },
-    retailPrice: {
-      type: Number,
-      required: false,
-    },
-    casePrice: {
+    price: {
       type: Number,
       required: true,
+    },
+    numInStock: {
+      type: Number,
+      required: true,
+    },
+    images: {
+      type: [ProductImageSchema],
+      required: true,
+    },
+    videoUrl: {
+      type: String,
+      required: false,
     },
     manufacturer: {
       type: String,
+      required: false,
+    },
+    duration: {
+      type: Number,
       required: false,
     },
     colors: {
       type: [String],
       required: false,
     },
-    videoUrl: {
-      type: String,
-      required: false,
-    },
-    type: {
-      type: String,
+    effects: {
+      type: [String],
       required: false,
     },
     numShots: {
       type: Number,
       required: false,
-    },
-    casePacking: {
-      type: Number,
-      required: true,
-    },
-    effects: {
-      type: [String],
-      required: false,
-    },
-    numInStock: {
-      type: Number,
-      required: true,
     },
   },
   { collection: 'products' }
